f7965d55fdab0dccba3146db745a915dccd6a1eb
Michael Powe <powem@ctpowe.net>
2022-10-24 11:50:55 -0400

    updated to write to file

--- json-parser.py
+++ json-parser.py
@@ -11,6 +11,7 @@ import urllib.parse
 
 GRIFFON_DATA = r"c:\Users\micha\Dropbox\src\python\json\data\AssuranceTraining.json"
 URL_DATA = r"c:\Users\micha\Dropbox\src\python\json\data\assurance-urls.txt"
+DATA_LIST = []
 
 f = open(GRIFFON_DATA, "r")
 w = open(URL_DATA, 'w')
@@ -21,10 +22,17 @@ length = len(json_data['events']) - 1
 for k in range(length):
     try:
         if 'hitUrl' in json_data['events'][k]['payload']['ACPExtensionEventData']:
-            print(urllib.parse.unquote(json_data['events'][k]['payload']['ACPExtensionEventData'].get('hitUrl')))
+            item = urllib.parse.unquote(json_data['events'][k]['payload']['ACPExtensionEventData'].get('hitUrl'))
+            print(item)
+            DATA_LIST.append(item)
     except KeyError:
         print(">>>>>>>>>> <<<<<<<<<<")
         print(">>>>>>>>>> key error, `ACPExtensionEventData` not found <<<<<<<<<<")
         print(">>>>>>>>>>", json_data['events'][k]['payload'], "<<<<<<<<<<")
         print(">>>>>>>>>> <<<<<<<<<<")
 f.close()
+
+for i in DATA_LIST:
+    w.write(i + "\n")
+
+w.close()
