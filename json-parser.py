33fe5b85ee5a3903391b5ab122297872249eab3a
Michael Powe <powem@ctpowe.net>
2022-10-24 11:39:31 -0400

    initial commit

---
+++ json-parser.py
@@ -0,0 +1,30 @@
+# Extract Adobe Analytics data strings from Adobe Assurance (Project Griffon) logs
+# 24 Oct 2022 11:18
+# Michael Powe
+
+import json
+import urllib.parse
+
+# from pprint import pprint
+
+# (json_data['events'][3]['payload']['ACPExtensionEventData']['hitUrl'])
+
+GRIFFON_DATA = r"c:\Users\micha\Dropbox\src\python\json\data\AssuranceTraining.json"
+URL_DATA = r"c:\Users\micha\Dropbox\src\python\json\data\assurance-urls.txt"
+
+f = open(GRIFFON_DATA, "r")
+w = open(URL_DATA, 'w')
+json_data = json.load(f)
+
+length = len(json_data['events']) - 1
+
+for k in range(length):
+    try:
+        if 'hitUrl' in json_data['events'][k]['payload']['ACPExtensionEventData']:
+            print(urllib.parse.unquote(json_data['events'][k]['payload']['ACPExtensionEventData'].get('hitUrl')))
+    except KeyError:
+        print(">>>>>>>>>> <<<<<<<<<<")
+        print(">>>>>>>>>> key error, `ACPExtensionEventData` not found <<<<<<<<<<")
+        print(">>>>>>>>>>", json_data['events'][k]['payload'], "<<<<<<<<<<")
+        print(">>>>>>>>>> <<<<<<<<<<")
+f.close()
